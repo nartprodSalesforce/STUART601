d8d21ef61fd672c83fedfb0c990d8354bc6b2fda
Andres Perez <aperez@salesforce.com>
2018-11-02 07:24:13 -0400

    EX 02-06

--- force-app/Exercises/main/aura/StudentTile/StudentTileController.js
+++ force-app/Exercises/main/aura/StudentTile/StudentTileController.js
@@ -1,5 +1,9 @@
 ({
-    onStudentClick : function(component, event, helper) {
-        alert(component.get('v.student').Name);
-    }
+	onStudentClick: function (component, event, helper) {
+		//alert(component.get('v.student').Name);
+		//debugger;
+		var student = component.get('v.student');
+		console.log('Name', student.Name);
+		console.log('Photo', student.PhotoUrl);
+	}
 })
